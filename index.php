888e053d2c1527ff59686a58491c90e1dc178a79
Rustam Safin <rustiksnegovik@gmail.com>
2016-02-18 21:53:27 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Желаем приятной работы! Хостинг-провайдер BeGet</title>
+    <style type="text/css">
+        p {
+            font-size: 13px;
+        }
+        ul {
+            font-size: 13px;
+        }
+
+        a:hover{
+            text-decoration:none;
+        }
+
+        a.a1 {
+            font-family: tahoma,verdana;
+            font-size: 13px;
+            margin-left: 12px;
+            color: black;
+            font-weight: bold;
+        }
+        a.a2 {
+            font-family: tahoma,verdana;
+            font-size: 13px;
+            align:left;
+            color: black;
+            font-weight: bold;
+        }
+
+        h1	{font-size:150%; margin:0 0 -0.7em 0; font-weight:normal; color: #215DC6;}
+
+        a {
+            font-family: tahoma,verdana;
+            font-size: 13px;
+            color: black;
+            font-weight: bold;
+        }
+
+        .mail{
+            font-size: 13px;
+            color: black;
+        }
+
+        .mail a{
+            font-family: tahoma,verdana;
+            font-size: 13px;
+            color: black;
+            font-weight: bold;
+        }
+
+    </style>
+</head>
+
+<body style="margin:0px; padding:0px; background: url(https://static.beget.ru/img/bg.png) repeat-x;">
+<div style="position: relative; z-index:1;">
+    <table width="100%" height="100%" >
+
+        <tr>
+            <td>
+                <center>
+                    <table border="0" style="width:800px; height:100%;">
+                        <tr>
+                            <td>
+
+                                <center>
+                                    <table style="width:630px; height:100%;">
+                                        <tr>
+                                            <td align="left"><br>
+                                                <center>
+                                                    <table border="0" style="width:800px; height:100%; ">
+                                                        <tr>
+                                                            <td valign="top" align="left"> <a href="http://www.beget.ru" border="0"><img src="https://static.beget.ru/img/logo.png" border="0"></a></td>
+                                                            <td align="left"><a class="a1" href="http://www.beget.ru/princip.html">О компании</a>&nbsp;<a class="a1" href="http://www.beget.ru/host.html">Услуги</a>&nbsp;<a  class="a1" href="http://www.beget.ru/contact.html">Контакты</a>&nbsp;<a href="http://cp.beget.ru" class="a1">В панель управления</a></td>
+
+                                                        </tr>
+                                                    </table>
+                                                </center>
+                                                <br><br>
+                                                <h1>Здравствуйте, уважаемый пользователь.</h1>
+                                                <br>
+                                                <p>Новый сайт успешно создан и подготовлен к работе.</p>
+
+                                                <p>Если у Вас возникли вопросы, Вы можете обратиться в любую из служб нашей компании:
+                                                    через e-mail, по телефону, по icq или через Панель Управления Аккаунтом.</p>
+                                                <br>
+                                                <table class="mail">
+                                                    <tr>
+                                                        <td>Техническая поддержка</td><td>-</td><td><a href="mailto:support@beget.ru">support@beget.ru</a></td>
+                                                    </tr>
+                                                    <!--<tr>
+                                                        <td>Клиентская служба</td><td>-</td><td><a href="mailto:clients@beget.ru">clients@beget.ru</a></td>
+                                                    </tr>-->
+                                                    <tr>
+                                                        <td>Финансовая служба</td><td>-</td><td><a href="mailto:bills@beget.ru">bills@beget.ru</a></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Информационная служба</td><td>-</td><td><a href="mailto:info@beget.ru">info@beget.ru</a></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Руководство компании</td><td>-</td><td><a href="mailto:manager@beget.ru">manager@beget.ru</a></td>
+                                                    </tr>
+                                                </table>
+                                                <br>
+
+
+                                                <p>Мы благодарим Вас за то, что Вы выбрали наши услуги.<br> Наша команда работает для Вас!</p>
+                                                <hr noshade size="1" />
+
+                                                <p class="sign">Тел.: <span id="phone">8 (800) 700-06-08</span>  © 2007-<?php echo date('Y');?>, ООО "Бегет"<br/>
+                                                    ICQ: <span id="icq">580-278-701</span>
+
+                                                </p>
+                                            </td>
+                                        </tr>
+                                    </table>
+
+                                </center>
+
+                            </td>
+                        </tr>
+                    </table>
+                </center>
+            <td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
